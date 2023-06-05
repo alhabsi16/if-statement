82c748d84b028f21cbbc2f417c1c36b349aa71a9
alhabsi16 <mxmx8283@gmail.com>
2023-06-05 10:38:35 +0400

    Grade

--- Program.cs
+++ Program.cs
@@ -4,17 +4,38 @@
     {
         static void Main(string[] args)
         {
-            int a = 5, b = 1;
-           
+            int a = 90, b = 80, c = 70, d = 60, f = 50 ;
+            int grade;
+            string name;
 
-            if ( a == b || a > b )
+            Console.Write("Enter Name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter Grade: ");
+            grade = int.Parse(Console.ReadLine());
+
+
+            if ( grade >= a  )
             {
-                Console.WriteLine("condition met");
+                Console.WriteLine( name + " get A in Math");
+            }
+            else if ( grade >= b )
+            {
+                Console.WriteLine(name + " get B in Math");
+            }
+            else if( grade >= c )
+            {
+                Console.WriteLine(name + " get C in Math");
+            }
+            else if (grade >= d)
+            {
+                Console.WriteLine(name + " get D in Math");
             }
             else
             {
-                Console.WriteLine("condition is not met");
+                Console.WriteLine(name + " get F in Math");
             }
+           
         }
     }
 }
