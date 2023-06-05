39e480c09dd2cb7400522ba97f4ef0f0edafa2cb
alhabsi16 <mxmx8283@gmail.com>
2023-06-05 10:10:02 +0400

    if V1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace if_statment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b = 1;
+           
+
+            if ( a == b || a > b )
+            {
+                Console.WriteLine("condition met");
+            }
+            else
+            {
+                Console.WriteLine("condition is not met");
+            }
+        }
+    }
+}
